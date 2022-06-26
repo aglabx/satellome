ad4a4d695b84ec282d8afd7aa9345f135b912cc4
Aleksey Komissarov <ad3002@gmail.com>
2022-06-26 19:10:27 +0300

    add parser

---
+++ trf_parse_raw.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#@created: 10.03.2019
+#@author: Aleksey Komissarov
+#@contact: ad3002@gmail.com
+
+import sys
+from trseeker.models.trf_model import TRModel
+from trseeker.seqio.trf_file import TRFFileIO
+import argparse
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Parse TRF output.')
+    parser.add_argument('-i','--input', help='Input file', required=True)
+    parser.add_argument('-o','--output', help='Output file', required=True)
+    parser.add_argument('-p','--project', help='Project', required=True)
+    args = vars(parser.parse_args())
+
+    input_file = args["input"]
+    output_file = args["output"]
+    project = args["project"]
+    
+    reader = TRFFileIO()
+    trf_id = 1
+    trf_id = reader.parse_to_file(input_file, output_file, trf_id=trf_id, project=project)
+    
